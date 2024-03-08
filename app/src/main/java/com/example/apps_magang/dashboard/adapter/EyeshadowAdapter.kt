1af73b934d4781421b23eb8af393b5dded1e9ec3
Alysa Syakirah <alysasyakirah0405@gmail.com>
2024-03-08 17:32:41 +0700

    feat: update ui dashboard

--- app/src/main/java/com/example/apps_magang/dashboard/adapter/EyeshadowAdapter.kt
+++ app/src/main/java/com/example/apps_magang/dashboard/adapter/EyeshadowAdapter.kt
@@ -55,7 +55,7 @@ class EyeshadowAdapter(
         val item = list[position]
 
         holder.tvProduct.text = item.name ?: ""
-        holder.tvBrand.text = item.brand ?: ""
+        holder.tvBrand.text = item.brand?.toUpperCase() ?: ""
 
         Glide.with(context)
             .load(item.imageLink)
